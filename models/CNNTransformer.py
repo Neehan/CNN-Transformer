23cd99e5261f8b16ea79117f2d1eecba3fb4044d
Adib Hasan <nzxcvb5@gmail.com>
2023-12-06 10:39:11 -0500

    added new embedding for CNN-Transformer

--- models/CNNTransformer.py
+++ models/CNNTransformer.py
@@ -26,24 +26,37 @@ class Model(nn.Module):
         self.pred_len = configs.pred_len
         # self.enc_in = configs.enc_in # encoder sequence length
 
+        seq_len = int((((configs.seq_len - 8 + 1) // 6) - 3 + 1) // 2)
+
         self.cnn = nn.Sequential(
             nn.Conv1d(
-                in_channels=1, out_channels=4, kernel_size=3, stride=1, padding=1
+                in_channels=1, out_channels=16, kernel_size=8, stride=1, padding=0
+            ),  # output 8 channel x
+            nn.BatchNorm1d(16),
+            nn.ReLU(),
+            nn.AvgPool1d(kernel_size=6, stride=6),
+            nn.Conv1d(
+                in_channels=16, out_channels=24, kernel_size=3, stride=1, padding=0
             ),  # output 4 channel x enc_in
-            nn.GELU(),
+            nn.ReLU(),
             nn.AvgPool1d(kernel_size=2, stride=2),
             nn.Flatten(),
-            nn.Linear(int(configs.enc_in // 2) * 4, configs.enc_in),
+            nn.Linear(seq_len * 24, configs.seq_len),
         )
 
         self.transformer = VanillaTransformer(configs)
 
+        for m in self.modules():
+            if isinstance(m, nn.Conv1d):
+                nn.init.kaiming_normal_(
+                    m.weight, mode="fan_in", nonlinearity="leaky_relu"
+                )
+
     def forecast(self, x_enc, x_mark_enc, x_dec, x_mark_dec):
         # Embedding
-        print(x_enc.shape)
-        batch_size, num_vars, length = x_enc.shape
+        batch_size, length, num_vars = x_enc.shape
         x_enc = self.cnn(x_enc.view(batch_size * num_vars, 1, length))
-        x_enc = x_enc.view(batch_size, num_vars, -1)
+        x_enc = x_enc.view(batch_size, -1, num_vars)
         return self.transformer(x_enc, x_mark_enc, x_dec, x_mark_dec)
 
     def imputation(self, x_enc, x_mark_enc, x_dec, x_mark_dec, mask):
@@ -61,6 +74,6 @@ class Model(nn.Module):
             self.task_name == "long_term_forecast"
             or self.task_name == "short_term_forecast"
         ):
-            return self.transformer.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
+            return self.forecast(x_enc, x_mark_enc, x_dec, x_mark_dec)
         else:
             return self.transformer(x_enc, x_mark_enc, x_dec, x_mark_dec, mask)
